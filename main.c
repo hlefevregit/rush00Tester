2e6b9552a6ee9285e3d956f12200b487e759854a
hlefevregit <hlefevre@DESKTOP-D6QEKJI>
2024-03-17 12:14:39 +0100

    tester for all the bonus added

--- main.c
+++ main.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hulefevr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hlefevre <hlefevre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 10:54:41 by hulefevr          #+#    #+#             */
-/*   Updated: 2024/03/16 12:15:15 by hulefevr         ###   ########.fr       */
+/*   Updated: 2024/03/17 12:06:07 by hlefevre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	rush_test(int x, int y);
+void	rush_test00(int x, int y);
+void	rush_test01(int x, int y);
+void	rush_test02(int x, int y);
+void	rush_test03(int x, int y);
+void	rush_test04(int x, int y);
 
 void	rush(int x, int y);
 
-int	main(void)
+int	getX(int x)
 {
-	int	x;
-	int	y;
-
-	printf("Welcome to Rush00, you will test the exercise 3\n");
 	printf("Please enter a value for x : \n");
 	char *lin_x = NULL;
 	size_t len_x = 0;
@@ -35,6 +35,11 @@ int	main(void)
 		return (0);
 	}
 	printf("The value for x is : %i\n", x);
+	return (x);
+}
+
+int	getY(int y)
+{
 	printf("Please enter a value for y : \n");
 	char	*lin_y = NULL;
 	size_t	len_y = 0;
@@ -47,9 +52,116 @@ int	main(void)
                 return (0);
         }
 	printf("The value of y is : %i\n", y);
+	return (y);
+}
+
+void	doRush00(void)
+{
+	int	x;
+	int	y;
+
+	x = getX(x);
+	y = getY(y);
+
 	printf("The expected return of the exercise is : \n");
-	rush_test(x, y);
+	rush_test00(x, y);
 	printf("Your return is : \n");
 	rush(x, y);
+}
+
+void	doRush01(void)
+{
+	int	x;
+	int	y;
+
+	x = getX(x);
+	y = getY(y);
+
+	printf("The expected return of the exercise is : \n");
+	rush_test01(x, y);
+	printf("Your return is : \n");
+	rush(x, y);
+}
+
+void	doRush02(void)
+{
+	int	x;
+	int	y;
+
+	x = getX(x);
+	y = getY(y);
+
+	printf("The expected return of the exercise is : \n");
+	rush_test02(x, y);
+	printf("Your return is : \n");
+	rush(x, y);
+}
+
+void	doRush03(void)
+{
+	int	x;
+	int	y;
+
+	x = getX(x);
+	y = getY(y);
+
+	printf("The expected return of the exercise is : \n");
+	rush_test03(x, y);
+	printf("Your return is : \n");
+	rush(x, y);
+}
+
+void	doRush04(void)
+{
+	int	x;
+	int	y;
+
+	x = getX(x);
+	y = getY(y);
+
+	printf("The expected return of the exercise is : \n");
+	rush_test04(x, y);
+	printf("Your return is : \n");
+	rush(x, y);
+}
+
+int	main(void)
+{
+	printf("Welcome to Rush00\n");
+	printf("To be sure that the test will work, please turn your file you want to test in rushXX.c and keep the prototype of your function in [void rush(int x, int y)], you can only test one rush at once");
+	printf("Thank you for taking part of the hulefevr rise, he'll be soon your best guru");
+	printf("Choose a rush subject : \n");
+	printf("Rush00 : [0]\n");
+	printf("Rush01 : [1]\n");
+	printf("Rush02 : [2]\n");
+	printf("Rush03 : [3]\n");
+	printf("Rush04 : [4]\n");
+	char *rushValue;
+	size_t rushValue_len;
+	ssize_t rushValue_size;
+	rushValue_size = getline(&rushValue, &rushValue_len, stdin);
+	
+	switch(atoi(rushValue))
+	{
+		case 0:
+			doRush00();
+			break;
+		case 1:
+			doRush01();
+			break;
+		case 2:
+			doRush02();
+			break;
+		case 3:
+			doRush03();
+			break;
+		case 4:
+			doRush04();
+			break;
+		default:
+			printf("Your request seems to be wrong, please enter a valid number beetwen 0 and 4");
+			break;
+	}
+
 	return (0);
 }
--- rush_test.c
+++ rush_test.c
@@ -3,16 +3,121 @@
 /*                                                        :::      ::::::::   */
 /*   rush_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hulefevr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hlefevre <hlefevre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 10:57:31 by hulefevr          #+#    #+#             */
-/*   Updated: 2024/03/16 12:15:43 by hulefevr         ###   ########.fr       */
+/*   Updated: 2024/03/17 12:13:06 by hlefevre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	rush_test(int x, int y)
+void	rush_test00(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j <= y)
+	{
+		i = 0;
+		while (++i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == y))
+				ft_putchar('o');
+			else if ((i != 1 && i != x) && (j == 1 || j == y))
+				ft_putchar('-');
+			else if ((i == x && j == 1) || (i == 1 && j == y))
+				ft_putchar('o');
+			else if ((j != 1 && j != y) && (i == 1 || i == x))
+				ft_putchar('|');
+			else if ((i != 1 && i != x) && (j != 1 && j != y))
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+		j++;
+	}
+}
+
+void	rush_test01(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	while (++j <= y)
+	{
+		i = 0;
+		while (++i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == y))
+				ft_putchar('/');
+			else if ((i == x && j == 1) || (i == 1 && j == y))
+				ft_putchar('\\');
+			else if ((i != 1 && i != x) && (j == 1 || j == y))
+				ft_putchar('*');
+			else if ((j != 1 && j != y) && (i == 1 || i == x))
+				ft_putchar('*');
+			else if ((i != 1 && i != x) && (j != 1 && j != y))
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+	}
+}
+
+void	rush_test04(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	while (++j <= y)
+	{
+		i = 0;
+		while (++i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == y))
+				ft_putchar('A');
+			else if ((i != 1 && i != x) && (j == 1 || j == y))
+				ft_putchar('B');
+			else if ((i == x && j == 1) || (x == 1 && j == y))
+				ft_putchar('C');
+			else if ((j != 1 || j != y) && (i == 1 || i == x))
+				ft_putchar('B');
+			else if ((i != 1 && i != x) && (j != 1 && j != y))
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+	}
+}
+
+void	rush_test02(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	while (++j <= y)
+	{
+		i = 0;
+		while (++i <= x)
+		{
+			if (j == 1 && (x == 1 || x == y))
+				ft_putchar('A');
+			else if ((i != 1 && i != x) && (j == 1 || j == y))
+				ft_putchar('B');
+			else if ((j == y) && (i == 1 || i == x))
+				ft_putchar('C');
+			else if ((j != 1 || j != y) && (i == 1 || i == x))
+				ft_putchar('B');
+			else if ((i != 1 && i != x) && (j != 1 && j != y))
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+	}
+}
+
+void	rush_test03(int x, int y)
 {
 	int	i;
 	int	j;
@@ -31,6 +136,35 @@ void	rush_test(int x, int y)
 				ft_putchar('C');
 			else if ((j != 1 || j != y) && (i == 1 || i == x))
 				ft_putchar('B');
+			else if ((i != 1 && i != x) && (j != 1 && j != y))
+				ft_putchar(' ');
+		}
+		ft_putchar('\n');
+	}
+}
+
+
+
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	while (++j <= y)
+	{
+		i = 0;
+		while (++i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == y))
+				ft_putchar('/');
+			else if ((i != 1 && i != x) && (j == 1 || j == y))
+				ft_putchar('*');
+			else if ((i == x && j == 1) || (i == 1 && j == y ))
+				ft_putchar('\\');
+			else if ((j != 1 && j != y) && (i == 1 || i == x))
+				ft_putchar('*');
 			else if ((i != 1 && i != x) && (j != 1 && j != y))
 				ft_putchar(' ');
 		}
